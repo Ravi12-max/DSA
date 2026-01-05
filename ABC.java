78efc7f6ff89b082088d0d353dbbee6b6be4bfd2
Ravi Ranjan kumar <ankitku.arayadav@gmail.com>
2026-01-05 17:56:35 +0530

    Add ABCattern class to print pattern of letters

---
+++ ABC.java
@@ -0,0 +1,12 @@
+class ABCattern {
+    public static void main(String[] args) {
+       int n=4;
+       for(int i=0;i<=n;i++){
+           for(int j=0;j<=i;j++){
+               System.out.print((char)(j+65)+" ");
+           }
+           System.out.println();
+       }
+       
+    }
+}
